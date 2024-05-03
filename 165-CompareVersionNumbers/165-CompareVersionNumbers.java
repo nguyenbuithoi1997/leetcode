a6d33fc92ce85ef0b782fa1cf838bff7a0ab4156
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-03 09:43:49 +0700

    Update 165-CompareVersionNumbers.java

--- 165-CompareVersionNumbers/165-CompareVersionNumbers.java
+++ 165-CompareVersionNumbers/165-CompareVersionNumbers.java
@@ -1,21 +1,18 @@
-        String[] v2Revisions = version2.split("\\.");
-        
-        int n = Math.max(v1Revisions.length, v2Revisions.length);
-        for (int i = 0; i < n; i++) {
-            int v1 = i < v1Revisions.length ? Integer.parseInt(v1Revisions[i]) : 0;
-            int v2 = i < v2Revisions.length ? Integer.parseInt(v2Revisions[i]) : 0;
-            
-            if (v1 < v2) {
-        String[] v1Revisions = version1.split("\\.");
-    public int compareVersion(String version1, String version2) {
-class Solution {
-                return -1;
-            } else if (v1 > v2) {
-                return 1;
-            }
-        }
-        
-        return 0;
-    }
+publicÂ intÂ compareVersion(StringÂ version1,Â StringÂ version2)Â {
+  String[] v1Revisions = version1.split("\\.");
+  String[] v2Revisions = version2.split("\\.");
+  
+  int n = Math.max(v1Revisions.length, v2Revisions.length);
+  for (int i = 0; i < n; i++) {
+      int v1 = i < v1Revisions.length ? Integer.parseInt(v1Revisions[i]) : 0;
+      int v2 = i < v2Revisions.length ? Integer.parseInt(v2Revisions[i]) : 0;
+      
+      if (v1 < v2) {
+          return -1;
+      } else if (v1 > v2) {
+          return 1;
+      }
+  }
+  
+  return 0;
 }
-"
