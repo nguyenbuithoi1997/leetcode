a8000a342141f0e7ed2036710c58cf23c48d7ced
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-06 17:29:55 +0700

    Update file 5/6/2024, 5:29:55 PM

--- 2487-RemoveNodesFromLinkedList/2487-RemoveNodesFromLinkedList.java
+++ 2487-RemoveNodesFromLinkedList/2487-RemoveNodesFromLinkedList.java
@@ -1,22 +1,3 @@
-        head.next = null;
-        head = prevNode;
-
-        prevNode = head;
-        currentNode = head.next;
-        while(currentNode != null){
-            if(currentNode.val < prevNode.val){
-                currentNode = currentNode.next;
-            }
-            else{
-                ListNode nextNode = currentNode.next;
-                currentNode.next = prevNode;
-                prevNode = currentNode;
-                currentNode = nextNode;
-            }
-            
-        }
-        head.next = null;
-        head = prevNode;
-        return head;
-    }
+ 
+*
 [
