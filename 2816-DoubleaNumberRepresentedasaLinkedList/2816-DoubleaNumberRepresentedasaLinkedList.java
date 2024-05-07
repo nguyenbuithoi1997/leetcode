6b50f638e62cd56232b9b3d1693d2c4430c3009e
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-07 13:47:02 +0700

    Create file

---
+++ 2816-DoubleaNumberRepresentedasaLinkedList/2816-DoubleaNumberRepresentedasaLinkedList.java
@@ -0,0 +1,10 @@
+                left.val += 1;
+            }
+            right.val = (right.val * 2) % 10;
+            left = left.next;
+            right = right.next;
+        }
+        return head1.val > 0 ? head1 : head;
+    }
+}
+[1,8,9]
