968373bf8d3b17953d0a7ab6853a9b80829870e9
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-08 16:30:34 +0700

    Create file

---
+++ 506-RelativeRanks/506-RelativeRanks.java
@@ -0,0 +1,21 @@
+            int index = list.indexOf(search);
+            switch (index) {
+                case 0:
+                    result[i] = "Gold Medal";
+                    break;
+                case 1:
+                    result[i] = "Silver Medal";
+                    break;
+                case 2:
+                    result[i] = "Bronze Medal";
+            int search = score[i];
+        for (int i = 0; i < score.length; i++) {
+                    break;
+boxed().sorted(Collections.reverseOrder()).toList();
+        List<Integer> list = Arrays.stream(score).
+
+                default:
+                    result[i] = String.valueOf(index
++1);
+            }
+[
