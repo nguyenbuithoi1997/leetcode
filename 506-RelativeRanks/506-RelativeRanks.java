2856a53a9e82409e3d5af7f24be19ab8091e4a1e
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-08 16:42:32 +0700

    Update file 5/8/2024, 4:42:32 PM

--- 506-RelativeRanks/506-RelativeRanks.java
+++ 506-RelativeRanks/506-RelativeRanks.java
@@ -1,22 +1,22 @@
-            switch (rank) {
                 case 1:
                     ranks[i] = "Gold Medal";
                     break;
                 case 2:
                     ranks[i] = "Silver Medal";
                     break;
+            switch (rank) {
+            rank = n - rank;
+        String[] ranks = new String[n];
+
+        for (int i = 0; i < score.length; i++) {
+            int rank = Arrays.binarySearch(sortedScore, score[i]);
                 case 3:
                     ranks[i] = "Bronze Medal";
                     break;
-            rank = n - rank;
-        for (int i = 0; i < score.length; i++) {
-            int rank = Arrays.binarySearch(sortedScore, score[i]);
-
-        String[] ranks = new String[n];
-        Arrays.sort(sortedScore);
                 default:
                     ranks[i] = String.valueOf(rank);
+                   
+                     break;
             }
         }
-        return ranks;
 [
