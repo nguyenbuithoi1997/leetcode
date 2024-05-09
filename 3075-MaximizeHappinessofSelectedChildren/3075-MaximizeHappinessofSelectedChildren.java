439f6a6ec0fd35fd60859a397dc331be17f7a185
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-09 15:34:00 +0700

    Update file 5/9/2024, 3:34:00 PM

--- 3075-MaximizeHappinessofSelectedChildren/3075-MaximizeHappinessofSelectedChildren.java
+++ 3075-MaximizeHappinessofSelectedChildren/3075-MaximizeHappinessofSelectedChildren.java
@@ -1,14 +1,22 @@
-            dec++;
-        }
-        return ans;
-        for(int i=n-1;i>=n-k;i--){
-            ans += Math.max(0,happiness[i]-dec);
-        Arrays.sort(happiness);
-        int dec = 0,n=happiness.length;
-        long ans=0;
-class Solution {
-    public long maximumHappinessSum(int[] happiness, int k) {
-    }
+        if (low == high) 
+            return;
+        
+        int left = low - 1, right = high + 1, mid = low + high >> 1;
+        int x = nums[mid];
+        while (left < right) {
+            while (nums[++left] < x) continue;
+            while (nums[--right] > x) continue;
+            if (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+        } 
+        
+        if (right < k) 
+            quickSort(nums, right + 1, high, k);
+        else
+            quickSort(nums, low, right, k);
+    }   
 
-}
 [1,2,3]
