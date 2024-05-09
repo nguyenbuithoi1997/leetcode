7a37bc5cdcf9a0c79b59518bdc5e0728be7c2046
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-09 15:33:13 +0700

    Create file

---
+++ 3075-MaximizeHappinessofSelectedChildren/3075-MaximizeHappinessofSelectedChildren.java
@@ -0,0 +1,14 @@
+            dec++;
+        }
+        return ans;
+        for(int i=n-1;i>=n-k;i--){
+            ans += Math.max(0,happiness[i]-dec);
+        Arrays.sort(happiness);
+        int dec = 0,n=happiness.length;
+        long ans=0;
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+    }
+
+}
+[1,2,3]
