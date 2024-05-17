4516138b4b9c6acbab71c7ffbee542daf63b1985
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-17 17:32:47 +0700

    Create file

---
+++ Explore/Explore.java
@@ -0,0 +1,21 @@
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        
+        if(root == null) return null;
+
+ *         this.val = val;
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode() {}
+ *     TreeNode right;
+ *     TreeNode left;
+ *     int val;
+ * public class TreeNode {
+ * Definition for a binary tree node.
+/**
+[1,2,3,2,null,2,4]
