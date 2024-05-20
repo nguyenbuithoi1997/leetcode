e3050563b4df3e01d02d16682e1a7227b3362cd1
Nguyễn Bùi Thội <33334690+nguyenbuithoi1997@users.noreply.github.com>
2024-05-20 21:26:51 +0700

    Update file 5/20/2024, 9:26:50 PM

--- Explore/Explore.java
+++ Explore/Explore.java
@@ -1,21 +1,18 @@
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public TreeNode removeLeafNodes(TreeNode root, int target) {
+    public int subsetXORSum(int[] nums) {
+        // First create helper function, this function will help us to calculate
+        // when i'm including, when i'm not. The XOR of that array
+        return helper(nums, 0, 0); // nums, level, current XOR
+    }
+    public int helper(int nums[], int level, int currentXOR){
+        // base condition
+        if(level == nums.length) return currentXOR;
+        // creating include 
+        int include = helper(nums, level + 1, currentXOR^nums[level]);
+        // creating exclude
+        int exclude = helper(nums, level + 1, currentXOR);
         
-        if(root == null) return null;
-
- *         this.val = val;
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode() {}
- *     TreeNode right;
- *     TreeNode left;
- *     int val;
- * public class TreeNode {
- * Definition for a binary tree node.
-/**
-[1,2,3,2,null,2,4]
+        return include + exclude;
+    }
+}
+[1,3]
